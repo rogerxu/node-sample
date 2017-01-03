e32fbda79448f7b9d2f54c0897e3e1d8885f7a64
Roger Xu <roger.xu.2007@gmail.com>
2017-01-03 18:36:53 +0800

    Add exports

--- sample/events.js
+++ sample/events.js
@@ -1,11 +1,4 @@
-import { EventEmitter } from 'events';
-import util from 'util';
-
-function Person(name) {
-  this.name = name;
-}
-
-util.inherits(Person, EventEmitter);
+const Person = require('../lib/Person');
 
 const roger = new Person('Roger');
 roger.on('speak', function (said) {
