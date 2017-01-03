3dafd549f913c5cbec9e61e233540e2615385519
Roger Xu <roger.xu.2007@gmail.com>
2017-01-03 15:27:44 +0800

    Add readline sample

--- sample/readline.js
+++ sample/readline.js
@@ -1,7 +1,29 @@
 const readline = require('readline');
 
-var rl = readline.createInterface(process.stdin, process.stdout);
+const rl = readline.createInterface(process.stdin, process.stdout);
+const person = {
+  name: '',
+  sayings: [],
+};
 
 rl.question('What is your name? ', function (answer) {
-  console.log(answer);
+  person.name = answer;
+  rl.setPrompt(`What would ${person.name} say? `);
+  rl.prompt();
+
+  rl.on('line', function (saying) {
+    person.sayings.push(saying.trim());
+
+    if (saying.toLowerCase().trim() === 'exit') {
+      rl.close();
+    } else {
+      rl.setPrompt(`What else would ${person.name} say? ('exit' to leave) `);
+      rl.prompt();
+    }
+  });
+});
+
+rl.on('close', function () {
+  console.log('%s says %j', person.name, person.sayings);
+  process.exit();
 });
