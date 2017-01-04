f9160bfee0263ed7cc4adc8e5d6227f518854fb2
Roger Xu <roger.xu.2007@gmail.com>
2017-01-04 18:08:47 +0800

    Add express

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+app.use(function (req, res, next) {
+  console.log(`${req.method} request for '${req.url}'`);
+  next();
+});
+
+app.use(express.static('./public'));
+
+app.listen(8080);
+
+console.log('Express app running on port 8080');
+
+module.exports = app;
