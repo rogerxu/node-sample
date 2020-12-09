6ff694f70bacdd277102350c45a6ff1dcbca2355
Roger Xu <roger.xu@sap.com>
2020-12-09 18:31:23 +0800

    Add prettier

--- test/sample-spec.js
+++ test/sample-spec.js
@@ -2,9 +2,9 @@ const chai = require('chai');
 
 const expect = chai.expect;
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
+describe('Array', function () {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
       expect([1, 2, 3].indexOf(4)).to.equal(-1);
     });
   });
